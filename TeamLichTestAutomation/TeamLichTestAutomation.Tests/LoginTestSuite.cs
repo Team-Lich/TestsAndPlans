a3ebf78319484e4e54e0f4de9e7eacced4ce446f
Decho <dechomecho@gmail.com>
2015-12-15 21:35:50 +0200

    Cleaned the solution. Style cop errors: 0.

---
+++ TeamLichTestAutomation/TeamLichTestAutomation.Tests/LoginTestSuite.cs
@@ -0,0 +1,14 @@
+﻿namespace TeamLichTestAutomation.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class LoginTestSuite
+    {
+        [TestMethod]
+        public void TestLoginWithValidRegularUserCredentials()
+        {
+        }
+    }
+}

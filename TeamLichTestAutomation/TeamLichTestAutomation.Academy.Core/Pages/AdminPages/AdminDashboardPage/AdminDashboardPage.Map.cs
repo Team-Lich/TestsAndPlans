cdc11e0de5a0c2bd7ec7670b2f1cf6dd70d7928e
Decho <dechomecho@gmail.com>
2015-12-17 22:45:52 +0200

    Added folder and started working on Admin tests

---
+++ TeamLichTestAutomation/TeamLichTestAutomation.Academy.Core/Pages/AdminPages/AdminDashboardPage/AdminDashboardPage.Map.cs
@@ -0,0 +1,15 @@
+﻿namespace TeamLichTestAutomation.Academy.Core.Pages.AdminPages.AdminDashboardPage
+{
+    using ArtOfTest.WebAii.Controls.HtmlControls;
+
+    public partial class AdminDashboardPage
+    {
+        private HtmlAnchor UniversitiesButton
+        {
+            get
+            {
+                return this.Browser.Find.ByExpression<HtmlAnchor>("href=/Administration_Users/Universities");
+            }
+        }
+    }
+}
---
+++ TeamLichTestAutomation/TeamLichTestAutomation.Academy.Core/Pages/AdminPages/AdminDashboardPage/AdminDashboardPage.cs
@@ -0,0 +1,16 @@
+﻿namespace TeamLichTestAutomation.Academy.Core.Pages.AdminPages.AdminDashboardPage
+{
+    using ArtOfTest.WebAii.Core;
+
+    public partial class AdminDashboardPage :BasePage
+    {
+        public AdminDashboardPage(Browser browser) : base(browser)
+        {
+        }
+
+        public void ClickUniversitiesButton()
+        {
+            this.UniversitiesButton.Click();
+        }        
+    }
+}

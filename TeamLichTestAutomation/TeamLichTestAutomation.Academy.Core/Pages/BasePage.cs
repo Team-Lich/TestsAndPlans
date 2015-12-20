d7c778d2aadd07e1ca2f4bebcf54aecfee8fd4ce
Decho <dechomecho@gmail.com>
2015-12-20 18:51:53 +0200

    Added locator by id with wait in base page class

--- TeamLichTestAutomation/TeamLichTestAutomation.Academy.Core/Pages/BasePage.cs
+++ TeamLichTestAutomation/TeamLichTestAutomation.Academy.Core/Pages/BasePage.cs
@@ -1,5 +1,6 @@
 ﻿namespace TeamLichTestAutomation.Academy.Core.Pages
 {
+    using ArtOfTest.WebAii.Controls.HtmlControls;
     using ArtOfTest.WebAii.Core;
 
     public abstract class BasePage
@@ -17,6 +18,18 @@
             {
                 return this.browser;
             }
+        }    
+
+        public T LocateById<T>(string id) where T : HtmlControl
+        {
+            this.browser.WaitForElement(5000, id);
+            return this.browser.Find.ById<T>(id);
+        }
+
+        public T LocateById<T>(int timeout, string id) where T : HtmlControl
+        {
+            this.browser.WaitForElement(timeout, id);
+            return this.browser.Find.ById<T>(id);
         }
     }
 }

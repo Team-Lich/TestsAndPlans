315b80a6ef676debb9bed4cd0247f776fd0c4d56
Decho <dechomecho@gmail.com>
2015-12-20 18:33:57 +0200

    Kendo grid wrapper first file

---
+++ TeamLichTestAutomation/TeamLichTestAutomation.TestFramework.Core/LichKendoGridWrapper.cs
@@ -0,0 +1,29 @@
+﻿namespace TeamLichTestAutomation.TestFramework.Core
+{
+    using ArtOfTest.WebAii.ObjectModel;
+    using Telerik.TestingFramework.Controls.KendoUI;
+
+    public class LichKendoGridWrapper
+    {
+        private readonly KendoGrid grid;
+
+        public LichKendoGridWrapper(KendoGrid grid)
+        {
+            this.grid = grid;
+        }
+
+        public KendoGrid Grid 
+        {
+            get
+            {
+                return this.grid;
+            }
+        }
+
+        public bool Contains(string stringToSearch)
+        {
+            var listOfItems = this.grid.FindItems(i => i.InnerText == stringToSearch);
+            return listOfItems.Count > 0;
+        }
+    }
+}

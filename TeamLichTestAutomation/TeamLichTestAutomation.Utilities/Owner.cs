c48c017e157fabbb4df8c6f1ce37402f64bb47b6
Decho <dechomecho@gmail.com>
2015-12-21 21:37:23 +0200

    Owner enumeration added in order to be used by custom TestOwnerAttribute

---
+++ TeamLichTestAutomation/TeamLichTestAutomation.Utilities/Owner.cs
@@ -0,0 +1,11 @@
+﻿namespace TeamLichTestAutomation.Utilities
+{
+    public enum Owner
+    {
+        Ilvie = 1,
+        DechoDechev = 5,
+        Dimitar = 10,
+        Ivan = 15,
+        Yane = 20
+    }
+}

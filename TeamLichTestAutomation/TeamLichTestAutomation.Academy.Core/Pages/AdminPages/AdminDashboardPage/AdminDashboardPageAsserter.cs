4a0e1e404611ffbe9b311dc8538c3edfb0b7babc
decho <dechomecho@gmail.com>
2015-12-24 16:00:20 +0200

    Added AdminDashboardPageAsserter.cs class and implemented single assert to check if the current page is the admin page

---
+++ TeamLichTestAutomation/TeamLichTestAutomation.Academy.Core/Pages/AdminPages/AdminDashboardPage/AdminDashboardPageAsserter.cs
@@ -0,0 +1,16 @@
+﻿namespace TeamLichTestAutomation.Academy.Core.Pages.AdminPages.AdminDashboardPage
+{
+    using ArtOfTest.Common.UnitTesting;
+    using ArtOfTest.WebAii.Controls.HtmlControls;
+
+    public static class AdminDashboardPageAsserter
+    {
+        public static void AssertCurrentlyOnThePage(this AdminDashboardPage page)
+        {
+            var mainContainer = page.Browser.Find.ById<HtmlDiv>("MainContainer");
+            var heading = mainContainer.Find.ByExpression("tagname=h1").InnerText;
+
+            Assert.AreEqual("Администрация", heading);
+        }
+    }
+}

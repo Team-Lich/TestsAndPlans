5d7d9e484436ce9f75165528cc925b9e25cfbcf8
decho <dechomecho@gmail.com>
2015-12-24 13:51:50 +0200

    Added DeleteRow method to UniversitiesPage

--- TeamLichTestAutomation/TeamLichTestAutomation.Academy.Core/Pages/AdminPages/UniversitiesPage/UniversitiesPage.cs
+++ TeamLichTestAutomation/TeamLichTestAutomation.Academy.Core/Pages/AdminPages/UniversitiesPage/UniversitiesPage.cs
@@ -1,7 +1,11 @@
 ﻿namespace TeamLichTestAutomation.Academy.Core.Pages.AdminPages.UniversitiesPage
 {
+    using ArtOfTest.WebAii.Controls.HtmlControls;
     using ArtOfTest.WebAii.Core;
+    using System.Windows.Forms;
     using System.Drawing;
+    using System.Threading;
+    using System.Web;
     using Telerik.TestingFramework.Controls.KendoUI;
 
     public partial class UniversitiesPage : BasePage
@@ -25,9 +29,28 @@
             this.UpdateButton.Click();
         }
 
-        public void DeleteRow(string value, int column)
+        public void DeleteRow(KendoGrid grid, string value, int searchColumn)
         {
-            
+            var rows = grid.DataItems;
+            HtmlAnchor deleteButton = null;
+
+            foreach (var row in rows)
+            {
+                if (row[searchColumn].InnerText == value)
+                {
+                    deleteButton = row.Find.ByExpression<HtmlAnchor>("class=~k-grid-delete");
+                    deleteButton.ScrollToVisible();
+                    this.Browser.RefreshDomTree();
+                    var rec = deleteButton.GetRectangle();
+
+                    this.Browser.Desktop.Mouse.Click(MouseClickType.LeftClick, rec);
+                    Thread.Sleep(1000);
+
+
+                    this.Browser.Desktop.KeyBoard.KeyPress(Keys.Return);
+                    Manager.Current.Desktop.KeyBoard.KeyPress(Keys.Return);
+                }
+            }
         }
     }
 }

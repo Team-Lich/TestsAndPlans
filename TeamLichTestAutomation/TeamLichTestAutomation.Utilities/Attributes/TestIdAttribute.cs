84fefd6626497d3bcb30d52fe260db5517ab2bcc
havaliova <havaliova@gmail.com>
2016-01-07 22:04:45 +0200

    Edit test id attribute

--- TeamLichTestAutomation/TeamLichTestAutomation.Utilities/Attributes/TestIdAttribute.cs
+++ TeamLichTestAutomation/TeamLichTestAutomation.Utilities/Attributes/TestIdAttribute.cs
@@ -4,8 +4,23 @@
 
     public class TestIdAttribute : Attribute
     {
+        private int testId;
+
         public TestIdAttribute(int testId)
         {
+            this.TestId = testId;
+        }
+
+        public int TestId
+        {
+            get
+            {
+                return this.testId;
+            }
+            set
+            {
+                this.testId = value;
+            }
         }
     }
 }

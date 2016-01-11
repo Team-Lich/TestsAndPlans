e7b9500a9c5dea83bd391f80f60066b7a5b00294
Decho <dechomecho@gmail.com>
2016-01-11 21:03:07 +0200

    Implemented the FileSystemHelper class methods

--- TeamLichTestAutomation/TeamLichTestAutomation.Utilities/FileSystemHelper.cs
+++ TeamLichTestAutomation/TeamLichTestAutomation.Utilities/FileSystemHelper.cs
@@ -6,29 +6,36 @@
 
     public class FileSystemHelper
     {
-        public static bool FilePresentInUserDownloadsDirectory(string fileName)
+        public static bool FilePresentInUserDownloadsDirectory(string expectedFileName, string expectedFileExtension)
         {
-            bool result;
+            bool result = false;
 
             string userDirectoryPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads";
             var files = Directory.GetFiles(userDirectoryPath);
 
-            result = files.Contains(userDirectoryPath + "\\" + fileName);
+            foreach (string filePath in files)
+            {
+                if (filePath.Contains(expectedFileName) && filePath.Contains("." + expectedFileExtension))
+                {
+                    result = true;
+                }
+            }
 
             return result;
         }
 
         public static string GetExpectedFileName(string prefix)
         {
-            string fileNameFormat = "{0}_Export-{1}-{2}_{3}-";
+            string fileNameFormat = "{0}_Export_{1}-{2:00}-{3:00}_{4:00}";
             DateTime currentTime = DateTime.Now;
 
             int year = currentTime.Year;
             int month = currentTime.Month;
             int day = currentTime.Day;
             int hours = currentTime.Hour;
+            string result = string.Format(fileNameFormat, prefix, year, month, day, hours);
 
-            return string.Format(fileNameFormat, prefix, year, month, year, day, hours);
+            return result;
         }
     }
 }
